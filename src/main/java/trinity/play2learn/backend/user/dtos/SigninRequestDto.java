de3d07acd4feaea9b34c3b0792bd9521051b6d3d
felipedelzoppo <felipedelzoppo@gmail.com>
2025-06-11 13:07:28 -0300

    feat: cree los dto se signin. #26

---
+++ src/main/java/trinity/play2learn/backend/user/dtos/SigninRequestDto.java
@@ -0,0 +1,27 @@
+package trinity.play2learn.backend.user.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import trinity.play2learn.backend.user.models.Role;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SigninRequestDto {
+
+    @Email
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    @NotNull
+    private Role role;
+    
+}
---
+++ src/main/java/trinity/play2learn/backend/user/dtos/SigninResponseDto.java
@@ -0,0 +1,19 @@
+package trinity.play2learn.backend.user.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import trinity.play2learn.backend.user.models.Role;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SigninResponseDto {
+
+    private Long id;
+    private String token;
+    private Role role;
+    private String email;
+}
