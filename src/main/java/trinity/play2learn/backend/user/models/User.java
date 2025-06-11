f0bf821c57a1f950e2827764ff1a219ec34966f8
felipedelzoppo <felipedelzoppo@gmail.com>
2025-06-11 13:02:55 -0300

    feat: cree modelo User

---
+++ src/main/java/trinity/play2learn/backend/user/models/User.java
@@ -0,0 +1,50 @@
+package trinity.play2learn.backend.user.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User implements UserDetails { 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email; 
+
+    private String password; 
+
+    private Role role;
+
+    @Override
+    public boolean isAccountNonExpired() { return true; } 
+    @Override
+    public boolean isAccountNonLocked() { return true; }  
+    @Override
+    public boolean isCredentialsNonExpired() { return true; } 
+    @Override
+    public boolean isEnabled() { return true; }
+
+    @Override
+    public String getUsername() {
+        return email;
+    } 
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+    
+}
