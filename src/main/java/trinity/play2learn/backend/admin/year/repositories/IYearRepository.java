eed83054e968736e02bcd4efb0fe6e60edb8d011
Juan Pablo Dealbera <juanpidealbera@gmail.com>
2025-06-12 10:31:07 -0300

    fix repositoy year
    Removi el decorador de repositorio ya que el mismo esta de forma implicita al extender de Crud Repository

--- src/main/java/trinity/play2learn/backend/admin/year/repositories/IYearRepository.java
+++ src/main/java/trinity/play2learn/backend/admin/year/repositories/IYearRepository.java
@@ -3,14 +3,12 @@ package trinity.play2learn.backend.admin.year.repositories;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import trinity.play2learn.backend.admin.year.models.Year;
 
 /**
  * Repositorio JPA para acceder a los datos persistidos del modelo Year.
  */
-@Repository
 public interface IYearRepository extends CrudRepository<Year, Long> {
 
     /**
