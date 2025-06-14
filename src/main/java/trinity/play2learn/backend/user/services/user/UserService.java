1292ffe6c9872820acbf3166e5e05e8364db0a97
felipedelzoppo <felipedelzoppo@gmail.com>
2025-06-14 12:38:53 -0300

    feat: Cambie findUser para que devuelva la misma excepcion si el email o la contraseña no son correctos. #27

--- src/main/java/trinity/play2learn/backend/user/services/user/UserService.java
+++ src/main/java/trinity/play2learn/backend/user/services/user/UserService.java
@@ -1,13 +1,8 @@
 package trinity.play2learn.backend.user.services.user;
 
-import java.util.Optional;
-
-
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import lombok.AllArgsConstructor;
-import trinity.play2learn.backend.configs.exceptions.NotFoundException;
 import trinity.play2learn.backend.configs.exceptions.UnauthorizedException;
 import trinity.play2learn.backend.user.models.User;
 import trinity.play2learn.backend.user.repository.IUserRepository;
@@ -19,30 +14,27 @@ public class UserService implements IUserService {
     private final IUserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    private final String UNAUTHORIZED_MESSAGE = "Email or password is incorrect";
+
     //Devuelve el usuario si existe y la contraseña es correcta. De lo contrario lanza una excepcion.
     @Override
     public User findUserOrThrowException(String email, String password) {
 
-        Optional<User> optionalUser = userRepository.findByEmail(email);
+        User user = findUserOrThrowException(email);
 
-        validateEmail(optionalUser);
-        validatePassword(optionalUser , password);
-
-        return optionalUser.get();
+        checkIfPasswordMatchesOrThrowException(password, user.getPassword());
+       
+        return user;
     }
 
-    //Valida que exista un usuario con ese email en la base de datos
-    private void validateEmail(Optional<User> optionalUser) {
-        if (optionalUser.isEmpty()) {
-            throw new NotFoundException("User not found");
+    private User findUserOrThrowException(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UnauthorizedException(UNAUTHORIZED_MESSAGE));
+    }
+
+    private void checkIfPasswordMatchesOrThrowException(String loginPassword, String dbPassword) {
+        if (!passwordEncoder.matches(loginPassword, dbPassword)) {
+            throw new UnauthorizedException(UNAUTHORIZED_MESSAGE);
         }
     }
 
-    //Valida que la contraseña recibida coincida con la de la base de datos
-    private void validatePassword(Optional<User> optionalUser , String password) {
-
-        if (passwordEncoder.matches(optionalUser.get().getPassword() , password)) {
-            throw new UnauthorizedException("Password is incorrect");
-        }
-    }
 }
