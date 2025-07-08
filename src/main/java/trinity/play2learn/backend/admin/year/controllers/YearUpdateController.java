efc30a16f7fd848e43e8e28ccb05b90dd212452c
Juan Pablo Dealbera <juanpidealbera@gmail.com>
2025-07-08 12:56:52 -0300

    feat Update Year
    El mismo contiene:
    - YearUpdateRequestDto
    - Service
    - Controller

---
+++ src/main/java/trinity/play2learn/backend/admin/year/controllers/YearUpdateController.java
@@ -0,0 +1,42 @@
+package trinity.play2learn.backend.admin.year.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import trinity.play2learn.backend.admin.year.dtos.YearResponseDto;
+import trinity.play2learn.backend.admin.year.dtos.YearUpdateRequestDto;
+import trinity.play2learn.backend.admin.year.services.interfaces.IYearUpdateService;
+import trinity.play2learn.backend.configs.aspects.SessionRequired;
+import trinity.play2learn.backend.configs.response.BaseResponse;
+import trinity.play2learn.backend.configs.response.ResponseFactory;
+import trinity.play2learn.backend.user.models.Role;
+
+/**
+ * Controlador REST para gestionar los años académicos del sistema.
+ * Expone endpoints para la actualización de nuevos años.
+ */
+@RequestMapping("/admin/years")
+@RestController
+@AllArgsConstructor
+public class YearUpdateController {
+
+    private final IYearUpdateService yearUpdateService;
+
+    /**
+     * CU10 - Modifica un año académico existente.
+     *
+     * @param YearRequestDto yearDto Datos del año académico.
+     * @return ResponseEntity con el año creado y mensaje de éxito.
+     */
+    @PutMapping
+    @SessionRequired(role = Role.ROLE_ADMIN)
+    public ResponseEntity<BaseResponse<YearResponseDto>> update(@Valid @RequestBody YearUpdateRequestDto yearDto) {
+        return ResponseFactory.ok(yearUpdateService.cu10UpdateYear(yearDto), "Created succesfully");
+    }
+    
+}
---
+++ src/main/java/trinity/play2learn/backend/admin/year/services/interfaces/IYearUpdateService.java
@@ -0,0 +1,10 @@
+package trinity.play2learn.backend.admin.year.services.interfaces;
+
+import trinity.play2learn.backend.admin.year.dtos.YearUpdateRequestDto;
+import trinity.play2learn.backend.admin.year.dtos.YearResponseDto;
+
+public interface IYearUpdateService {
+    
+    public YearResponseDto cu10UpdateYear(YearUpdateRequestDto yearUpdateRequestDto);
+    
+}
