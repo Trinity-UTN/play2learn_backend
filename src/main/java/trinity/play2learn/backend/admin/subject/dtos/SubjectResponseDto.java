5d7d1386bf8a0dfda4dea0c178c62d35f9635da3
felipedelzoppo <felipedelzoppo@gmail.com>
2025-07-10 17:55:05 -0300

    feat: Cree los dtos de materia. #62

---
+++ src/main/java/trinity/play2learn/backend/admin/subject/dtos/SubjectResponseDto.java
@@ -0,0 +1,18 @@
+package trinity.play2learn.backend.admin.subject.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import trinity.play2learn.backend.admin.course.dtos.CourseResponseDto;
+import trinity.play2learn.backend.admin.teacher.dtos.TeacherResponseDto;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class SubjectResponseDto {
+    
+    private Long id;
+    private String name;
+    private CourseResponseDto course;
+    private TeacherResponseDto teacher;
+}
