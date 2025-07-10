a20724426ee8a215daafad0b6878ee56b5b42559
felipedelzoppo <felipedelzoppo@gmail.com>
2025-07-10 17:28:09 -0300

    feat: Cree getTeacherById. #62

---
+++ src/main/java/trinity/play2learn/backend/admin/teacher/services/interfaces/IGetTeacherByIdService.java
@@ -0,0 +1,8 @@
+package trinity.play2learn.backend.admin.teacher.services.interfaces;
+
+import trinity.play2learn.backend.admin.teacher.models.Teacher;
+
+public interface IGetTeacherByIdService {
+    
+    Teacher getTeacherById(Long id);
+}
