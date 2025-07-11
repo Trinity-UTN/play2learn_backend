785895943744d16c1523c1abcbf947b1eadfada2
felipedelzoppo <felipedelzoppo@gmail.com>
2025-07-11 20:00:09 -0300

    feat: Cree endpoint para actualizar materia

---
+++ src/main/java/trinity/play2learn/backend/admin/subject/controllers/SubjectUpdateController.java
@@ -0,0 +1,28 @@
+package trinity.play2learn.backend.admin.subject.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import trinity.play2learn.backend.admin.subject.dtos.SubjectResponseDto;
+import trinity.play2learn.backend.admin.subject.dtos.SubjectUpdateRequestDto;
+import trinity.play2learn.backend.admin.subject.services.interfaces.ISubjectUpdateService;
+import trinity.play2learn.backend.configs.response.BaseResponse;
+import trinity.play2learn.backend.configs.response.ResponseFactory;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/admin/subjects")
+public class SubjectUpdateController {
+    
+    private final ISubjectUpdateService subjectService;
+
+    @PutMapping
+    public ResponseEntity<BaseResponse<SubjectResponseDto>> create(@Valid @RequestBody SubjectUpdateRequestDto subjectDto) {
+        return ResponseFactory.created(subjectService.cu29UpdateSubject(subjectDto), "Updated succesfully");
+    }
+
+}
