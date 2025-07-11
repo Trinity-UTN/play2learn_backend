bbaf864fedb86fff1ae2247629db613935a72dd0
felipedelzoppo <felipedelzoppo@gmail.com>
2025-07-11 19:37:39 -0300

    feat: Agregue validacion de materia con nombre y curso repetido. #62

--- src/main/java/trinity/play2learn/backend/admin/subject/repositories/ISubjectRepository.java
+++ src/main/java/trinity/play2learn/backend/admin/subject/repositories/ISubjectRepository.java
@@ -2,8 +2,10 @@ package trinity.play2learn.backend.admin.subject.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 
+import trinity.play2learn.backend.admin.course.models.Course;
 import trinity.play2learn.backend.admin.subject.models.Subject;
 
 public interface ISubjectRepository extends CrudRepository<Subject , Long> {
     
+    Boolean existsByNameAndCourse(String name, Course course);
 }
---
+++ src/main/java/trinity/play2learn/backend/admin/subject/services/commons/ValidateSubjectService.java
@@ -0,0 +1,26 @@
+package trinity.play2learn.backend.admin.subject.services.commons;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import trinity.play2learn.backend.admin.course.models.Course;
+import trinity.play2learn.backend.admin.subject.repositories.ISubjectRepository;
+import trinity.play2learn.backend.admin.subject.services.interfaces.IValidateSubjectService;
+import trinity.play2learn.backend.configs.exceptions.ConflictException;
+
+@Service
+@AllArgsConstructor
+public class ValidateSubjectService implements IValidateSubjectService {
+    
+    private final ISubjectRepository subjectRepository;
+
+    @Override
+    public void subjectExistByNameAndCourse(String name , Course course) {
+
+        if (subjectRepository.existsByNameAndCourse(name, course)) {
+            throw new ConflictException("Subject with name " + name + " already exists in course " + course.getYear().getName() + " " +course.getName());
+        };
+            
+        
+    }
+}
