192810a301f316720f9aafaa7bd3898c6f080744
felipedelzoppo <felipedelzoppo@gmail.com>
2025-07-11 12:27:13 -0300

    feat: Movi getStudentsByCourse a modulo student

--- src/main/java/trinity/play2learn/backend/admin/student/services/interfaces/IGetStudentsByCourse.java
+++ src/main/java/trinity/play2learn/backend/admin/student/services/interfaces/IGetStudentsByCourse.java
@@ -1,10 +1,10 @@
-package trinity.play2learn.backend.admin.course.services.interfaces;
+package trinity.play2learn.backend.admin.student.services.interfaces;
 
 import java.util.List;
 
 import trinity.play2learn.backend.admin.student.models.Student;
 
-public interface ICourseGetStudents {
+public interface IGetStudentsByCourse {
     
     List<Student> getStudentsByCourseId(Long courseId);
 }
