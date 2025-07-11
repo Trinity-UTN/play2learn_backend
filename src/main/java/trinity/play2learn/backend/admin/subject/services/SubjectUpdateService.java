e953bb5abff2f80a4532bba1a9a77ac1c4a8db05
felipedelzoppo <felipedelzoppo@gmail.com>
2025-07-11 19:59:52 -0300

    feat: Cree servicio para actualizar materia. #66

---
+++ src/main/java/trinity/play2learn/backend/admin/subject/services/SubjectUpdateService.java
@@ -0,0 +1,59 @@
+package trinity.play2learn.backend.admin.subject.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import trinity.play2learn.backend.admin.course.models.Course;
+import trinity.play2learn.backend.admin.course.services.interfaces.ICourseGetByIdService;
+import trinity.play2learn.backend.admin.student.models.Student;
+import trinity.play2learn.backend.admin.student.services.interfaces.IGetStudentsByCourse;
+import trinity.play2learn.backend.admin.subject.dtos.SubjectResponseDto;
+import trinity.play2learn.backend.admin.subject.dtos.SubjectUpdateRequestDto;
+import trinity.play2learn.backend.admin.subject.mappers.SubjectMapper;
+import trinity.play2learn.backend.admin.subject.models.Subject;
+import trinity.play2learn.backend.admin.subject.repositories.ISubjectRepository;
+import trinity.play2learn.backend.admin.subject.services.interfaces.ISubjectUpdateService;
+import trinity.play2learn.backend.admin.subject.services.interfaces.IValidateSubjectService;
+import trinity.play2learn.backend.admin.teacher.models.Teacher;
+import trinity.play2learn.backend.admin.teacher.services.interfaces.IGetTeacherByIdService;
+
+@Service
+@AllArgsConstructor
+public class SubjectUpdateService implements ISubjectUpdateService {
+
+    private final ICourseGetByIdService courseGetByIdService;
+    private final IGetTeacherByIdService getTeacherByIdService;
+    private final IGetStudentsByCourse courseGetStudentsService;
+    private final ISubjectRepository subjectRepository;
+    private final IValidateSubjectService validateSubjectService;
+
+    @Override
+    public SubjectResponseDto cu29UpdateSubject(SubjectUpdateRequestDto subjectDto) {
+        
+        validateSubjectService.subjectExistById(subjectDto.getId()); //Lanza un NotFoundException si no se encuentra una materia con el ID proporcionado
+
+        Course course = courseGetByIdService.get(subjectDto.getCourseId()); //De no encontrar un curso con el ID proporcionado, lanza una excepción NotFoundException
+        
+        validateSubjectService.subjectExistByNameAndCourse(subjectDto.getName(), course); //Lanza un conflict exception si la materia ya existe en el curso
+
+        Teacher teacher = null;
+        if (subjectDto.getTeacherId() != null) {
+            teacher = getTeacherByIdService.getTeacherById(subjectDto.getTeacherId()); //De no encontrar un profesor con el ID proporcionado, lanza una excepción NotFoundException
+        }
+        //Si no se proporciona un ID de profesor, se establece en null
+
+        List<Student> students = new ArrayList<>(); //Lista vacia
+        if (!subjectDto.getOptional()) {
+            students = courseGetStudentsService.getStudentsByCourseId(subjectDto.getCourseId()); //Si la materia no es opcional, asigno a todos los estudiantes del curso a la misma.
+        }
+        //Si es opcional, no se asignan estudiantes.
+
+        Subject subjectToUpdate = SubjectMapper.toUpdateModel(subjectDto, course, teacher, students);
+
+        return SubjectMapper.toSubjectDto(subjectRepository.save(subjectToUpdate));
+    }
+    
+}
