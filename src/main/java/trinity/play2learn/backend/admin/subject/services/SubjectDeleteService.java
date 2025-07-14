6a29809d52f015853162f25a723dc058b22a5f43
felipedelzoppo <felipedelzoppo@gmail.com>
2025-07-14 12:52:54 -0300

    feat: endpoint para eliminar una materia. #75

---
+++ src/main/java/trinity/play2learn/backend/admin/subject/services/SubjectDeleteService.java
@@ -0,0 +1,30 @@
+package trinity.play2learn.backend.admin.subject.services;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import trinity.play2learn.backend.admin.subject.models.Subject;
+import trinity.play2learn.backend.admin.subject.services.interfaces.IFindSubjectByIdService;
+import trinity.play2learn.backend.admin.subject.services.interfaces.ISubjectDeleteService;
+import trinity.play2learn.backend.configs.exceptions.ConflictException;
+
+@Service
+@AllArgsConstructor
+public class SubjectDeleteService implements ISubjectDeleteService {
+    
+    private final IFindSubjectByIdService findSubjectByIdService;
+
+    @Override
+    public void cu30DeleteSubject(Long id) {
+
+        Subject subject = findSubjectByIdService.findByIdOrThrowException(id);
+
+        //No es posible eliminar una materia con estudiantes asociados
+        if (!subject.getStudents().isEmpty()) {
+            throw new ConflictException("Cannot delete subject with ID " + id + " because it has associated students.");
+        }
+
+        subject.delete();
+
+    }
+}
