ddde59a307d925ef9c912467bf6582ea2b9e4a4a
felipedelzoppo <felipedelzoppo@gmail.com>
2025-07-14 10:43:59 -0300

    feat: Endpoint GetTeacherById. #71

---
+++ src/main/java/trinity/play2learn/backend/admin/teacher/controllers/TeacherGetController.java
@@ -0,0 +1,27 @@
+package trinity.play2learn.backend.admin.teacher.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import trinity.play2learn.backend.admin.teacher.dtos.TeacherResponseDto;
+import trinity.play2learn.backend.admin.teacher.services.interfaces.ITeacherGetService;
+import trinity.play2learn.backend.configs.response.BaseResponse;
+import trinity.play2learn.backend.configs.response.ResponseFactory;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/admin/teachers")
+public class TeacherGetController {
+    
+    private final ITeacherGetService teacherGetService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BaseResponse<TeacherResponseDto>> getTeacherById(@PathVariable Long id) {
+        
+        return ResponseFactory.ok(teacherGetService.cu28GetTeacherById(id), "Ok");
+    }
+}
