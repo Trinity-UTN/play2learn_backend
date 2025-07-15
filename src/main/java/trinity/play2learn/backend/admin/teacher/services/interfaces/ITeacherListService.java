421d1b8416ec0467a9a2e2b3c895f21f79eb9c9e
Felipe Del Zoppo <114626945+FelipeDelZoppo@users.noreply.github.com>
2025-07-15 15:57:40 -0300

    71 Endpoints Get Teachers (#74)
    * feat: Endpoint GetTeacherById. #71
    
    * feat: Agregue sessionRequired en TeacherGetById. #71
    
    * feat: Endpoint ListTeachers. #71
    
    * feat: Endpoint ListTeachersPaginated. #71

---
+++ src/main/java/trinity/play2learn/backend/admin/teacher/services/interfaces/ITeacherListService.java
@@ -0,0 +1,10 @@
+package trinity.play2learn.backend.admin.teacher.services.interfaces;
+
+import java.util.List;
+
+import trinity.play2learn.backend.admin.teacher.dtos.TeacherResponseDto;
+
+public interface ITeacherListService {
+    
+    List<TeacherResponseDto> cu25ListTeachers();
+}
