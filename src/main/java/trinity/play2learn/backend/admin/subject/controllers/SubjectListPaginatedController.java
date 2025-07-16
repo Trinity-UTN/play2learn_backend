5999cbe9506e41869558abf402d390cc3d80670c
felipedelzoppo <felipedelzoppo@gmail.com>
2025-07-16 12:40:48 -0300

    feat: Listar materias con paginacion. #92

---
+++ src/main/java/trinity/play2learn/backend/admin/subject/controllers/SubjectListPaginatedController.java
@@ -0,0 +1,40 @@
+package trinity.play2learn.backend.admin.subject.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import trinity.play2learn.backend.admin.subject.dtos.SubjectResponseDto;
+import trinity.play2learn.backend.admin.subject.services.interfaces.ISubjectListPaginatedService;
+import trinity.play2learn.backend.configs.aspects.SessionRequired;
+import trinity.play2learn.backend.configs.response.BaseResponse;
+import trinity.play2learn.backend.configs.response.PaginatedData;
+import trinity.play2learn.backend.configs.response.ResponseFactory;
+import trinity.play2learn.backend.user.models.Role;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/admin/subjects")
+public class SubjectListPaginatedController {
+    
+    private final ISubjectListPaginatedService subjectListPaginatedService;
+
+    @GetMapping("/paginated")
+    @SessionRequired(roles = {Role.ROLE_ADMIN})
+    public ResponseEntity<BaseResponse<PaginatedData<SubjectResponseDto>>> listPaginated(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(name = "page_size", defaultValue = "10") int pageSize,
+            @RequestParam(name = "order_by", defaultValue = "id") String orderBy,
+            @RequestParam(name = "order_type", defaultValue = "asc") String orderType,
+            @RequestParam(required = false) String search,
+            @RequestParam(name = "filters", required = false) List<String> filters,
+            @RequestParam(name = "filtersValues", required = false) List<String> filtersValues
+    ) {
+        return ResponseFactory.paginated(subjectListPaginatedService.cu32ListSubjectsPaginated(page, pageSize, orderBy, orderType, search, filters, filtersValues),  "Ok");
+    }
+}
