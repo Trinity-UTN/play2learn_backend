b59618547f3aab8dc6d1ef991295e7c1e88eea71
Felipe Del Zoppo <114626945+FelipeDelZoppo@users.noreply.github.com>
2025-07-16 13:08:09 -0300

    feat: GetSubjectById. #93 (#94)

---
+++ src/main/java/trinity/play2learn/backend/admin/subject/controllers/SubjectGetController.java
@@ -0,0 +1,33 @@
+package trinity.play2learn.backend.admin.subject.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import trinity.play2learn.backend.admin.subject.dtos.SubjectResponseDto;
+import trinity.play2learn.backend.admin.subject.services.interfaces.ISubjectGetService;
+import trinity.play2learn.backend.configs.aspects.SessionRequired;
+import trinity.play2learn.backend.configs.response.BaseResponse;
+import trinity.play2learn.backend.configs.response.ResponseFactory;
+import trinity.play2learn.backend.user.models.Role;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/admin/subjects")
+public class SubjectGetController {
+    
+    private final ISubjectGetService subjectGetService;
+
+    @GetMapping("/{id}")
+    @SessionRequired(roles = {Role.ROLE_ADMIN})
+    public ResponseEntity<BaseResponse<SubjectResponseDto>> getById(@PathVariable Long id) {
+        
+        return ResponseFactory.ok(subjectGetService.cu33GetSubjectById(id), "Ok");
+    }
+    
+}
