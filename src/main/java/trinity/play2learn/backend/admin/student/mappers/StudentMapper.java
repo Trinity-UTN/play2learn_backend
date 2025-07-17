d36e319f7898180acdf43bcfcacaf2f30b0daeaf
felipedelzoppo <felipedelzoppo@gmail.com>
2025-07-17 17:22:35 -0300

    feat: Cree el metodo para mapear el estudiante a su dto simplificado. #69

--- src/main/java/trinity/play2learn/backend/admin/student/mappers/StudentMapper.java
+++ src/main/java/trinity/play2learn/backend/admin/student/mappers/StudentMapper.java
@@ -1,9 +1,12 @@
 package trinity.play2learn.backend.admin.student.mappers;
 
+import java.util.List;
+
 import trinity.play2learn.backend.admin.course.mappers.CourseMapper;
 import trinity.play2learn.backend.admin.course.models.Course;
 import trinity.play2learn.backend.admin.student.dtos.StudentRequestDto;
 import trinity.play2learn.backend.admin.student.dtos.StudentResponseDto;
+import trinity.play2learn.backend.admin.student.dtos.StudentSimplificatedResponse;
 import trinity.play2learn.backend.admin.student.dtos.StudentUpdateRequestDto;
 import trinity.play2learn.backend.admin.student.models.Student;
 import trinity.play2learn.backend.user.mapper.UserMapper;
@@ -42,5 +45,21 @@ public class StudentMapper {
             .user(model.getUser())
             .deletedAt(model.getDeletedAt())
             .build();
+    }
+
+    public static StudentSimplificatedResponse toSimplificatedDto (Student student) {
+        return StudentSimplificatedResponse.builder()
+            .id(student.getId())
+            .name(student.getName())
+            .lastname(student.getLastname())
+            .dni(student.getDni())
+            .build();
+    }
+
+    public static List<StudentSimplificatedResponse> toSimplificatedDtos (List<Student> students) {
+        return students
+            .stream()
+            .map(student -> toSimplificatedDto(student))
+            .toList();
     }
 }
