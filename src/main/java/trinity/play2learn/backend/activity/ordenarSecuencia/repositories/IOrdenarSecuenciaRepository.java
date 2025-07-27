e6d3d195204546104ba93db7829938add4cb6a7b
Felipe Del Zoppo <114626945+FelipeDelZoppo@users.noreply.github.com>
2025-07-27 16:21:56 -0300

    Borre comentarios

--- src/main/java/trinity/play2learn/backend/activity/ordenarSecuencia/repositories/IOrdenarSecuenciaRepository.java
+++ src/main/java/trinity/play2learn/backend/activity/ordenarSecuencia/repositories/IOrdenarSecuenciaRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import trinity.play2learn.backend.activity.ordenarSecuencia.models.OrdenarSecuencia;
 
 public interface IOrdenarSecuenciaRepository extends CrudRepository<OrdenarSecuencia, Long> {
-    // Aquí puedes agregar métodos específicos para manejar OrdenarSecuencia si es necesario
-    // Por ejemplo, encontrar por algún campo específico o realizar consultas personalizadas
+
     
 }
