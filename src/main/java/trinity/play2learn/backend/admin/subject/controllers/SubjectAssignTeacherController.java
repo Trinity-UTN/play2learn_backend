54fda50f02e05080782892b55f78b0572fb94bdb
Juan Pablo Dealbera <juanpidealbera@gmail.com>
2025-08-08 11:30:39 -0300

    fix Aspect to Annotation

--- src/main/java/trinity/play2learn/backend/admin/subject/controllers/SubjectAssignTeacherController.java
+++ src/main/java/trinity/play2learn/backend/admin/subject/controllers/SubjectAssignTeacherController.java
@@ -9,7 +9,7 @@ import lombok.AllArgsConstructor;
 import trinity.play2learn.backend.admin.subject.dtos.SubjectResponseDto;
 import trinity.play2learn.backend.admin.subject.services.interfaces.ISubjectAssignTeacherService;
 import trinity.play2learn.backend.admin.subject.services.interfaces.ISubjectUnassignTeacherService;
-import trinity.play2learn.backend.configs.aspects.SessionRequired;
+import trinity.play2learn.backend.configs.annotations.SessionRequired;
 import trinity.play2learn.backend.configs.messages.SuccessfulMessages;
 import trinity.play2learn.backend.configs.response.BaseResponse;
 import trinity.play2learn.backend.configs.response.ResponseFactory;
