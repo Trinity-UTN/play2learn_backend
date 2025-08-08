5ade4c3b7bc067233cc87ec80af9ddefe3ccc55c
Juan Pablo Dealbera <juanpidealbera@gmail.com>
2025-08-08 10:35:39 -0300

    fix Elimine la exception cuando selecciona un aspecto ya seleccionado

--- src/main/java/trinity/play2learn/backend/profile/avatar/models/TypeAspect.java
+++ src/main/java/trinity/play2learn/backend/profile/avatar/models/TypeAspect.java
@@ -1,33 +1,23 @@
 package trinity.play2learn.backend.profile.avatar.models;
 
-import trinity.play2learn.backend.configs.exceptions.ConflictException;
 import trinity.play2learn.backend.profile.profile.models.Profile;
 
 public enum TypeAspect {
     CUERPO {
         @Override
         public void assign(Profile profile, Aspect aspect) {
-            if (aspect.equals(profile.getSelectedBody())) {
-                throw new ConflictException("El aspecto ya está seleccionado");
-            }
             profile.setSelectedBody(aspect);
         }
     },
     REMERA {
         @Override
         public void assign(Profile profile, Aspect aspect) {
-            if (aspect.equals(profile.getSelectedShirt())) {
-                throw new ConflictException("El aspecto ya está seleccionado");
-            }
             profile.setSelectedShirt(aspect);
         }
     },
     SOMBRERO {
         @Override
         public void assign(Profile profile, Aspect aspect) {
-            if (aspect.equals(profile.getSelectedHat())) {
-                throw new ConflictException("El aspecto ya está seleccionado");
-            }
             profile.setSelectedHat(aspect);
         }
     };
