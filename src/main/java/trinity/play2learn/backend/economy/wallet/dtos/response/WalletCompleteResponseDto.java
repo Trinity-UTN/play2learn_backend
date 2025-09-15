37547f72ccf5b698bfb62cd114fa67c6f38d76ec
Juan Pablo Dealbera <juanpidealbera@gmail.com>
2025-09-15 09:38:35 -0300

    feat Get Wallet

--- src/main/java/trinity/play2learn/backend/economy/wallet/dtos/response/WalletCompleteResponseDto.java
+++ src/main/java/trinity/play2learn/backend/economy/wallet/dtos/response/WalletCompleteResponseDto.java
@@ -1,5 +1,7 @@
 package trinity.play2learn.backend.economy.wallet.dtos.response;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,12 +9,14 @@ import lombok.Data;
 @Data
 @Builder
 @AllArgsConstructor
-public class WalletResponseDto {
-
+public class WalletCompleteResponseDto {
+    
     private Long id;
 
     private Double balance;
-    
+
     private Double invertedBalance;
-    
+
+    private List<MovementResponseDto> movements;
+
 }
--- src/main/java/trinity/play2learn/backend/economy/wallet/mappers/WalletMapper.java
+++ src/main/java/trinity/play2learn/backend/economy/wallet/mappers/WalletMapper.java
@@ -1,5 +1,9 @@
 package trinity.play2learn.backend.economy.wallet.mappers;
 
+import java.util.List;
+
+import trinity.play2learn.backend.economy.wallet.dtos.response.MovementResponseDto;
+import trinity.play2learn.backend.economy.wallet.dtos.response.WalletCompleteResponseDto;
 import trinity.play2learn.backend.economy.wallet.dtos.response.WalletResponseDto;
 import trinity.play2learn.backend.economy.wallet.models.Wallet;
 
@@ -10,6 +14,15 @@ public class WalletMapper {
             .id(wallet.getId())
             .balance(wallet.getBalance())
             .invertedBalance(wallet.getInvertedBalance())
+            .build();
+    }
+
+    public static WalletCompleteResponseDto toCompleteDto(Wallet wallet, List<MovementResponseDto> movements) {
+        return WalletCompleteResponseDto.builder()
+            .id(wallet.getId())
+            .balance(wallet.getBalance())
+            .invertedBalance(wallet.getInvertedBalance())
+            .movements(movements)
             .build();
     }
     
