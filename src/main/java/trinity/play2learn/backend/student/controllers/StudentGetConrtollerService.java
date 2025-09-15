383a5308299748339414194c2e391e024de812ef
felipedelzoppo <felipedelzoppo@gmail.com>
2025-09-15 20:23:32 -0300

    feat: Agregue SessionRequired. #227

--- src/main/java/trinity/play2learn/backend/student/controllers/StudentGetConrtollerService.java
+++ src/main/java/trinity/play2learn/backend/student/controllers/StudentGetConrtollerService.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 import trinity.play2learn.backend.admin.student.dtos.StudentResponseDto;
+import trinity.play2learn.backend.configs.annotations.SessionRequired;
 import trinity.play2learn.backend.configs.annotations.SessionUser;
 import trinity.play2learn.backend.configs.messages.SuccessfulMessages;
 import trinity.play2learn.backend.configs.response.BaseResponse;
 import trinity.play2learn.backend.configs.response.ResponseFactory;
 import trinity.play2learn.backend.student.services.interfaces.IStudentGetByTokenService;
+import trinity.play2learn.backend.user.models.Role;
 import trinity.play2learn.backend.user.models.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +25,7 @@ public class StudentGetConrtollerService {
     private final IStudentGetByTokenService studentGetByTokenService;
 
     @GetMapping
+    @SessionRequired(roles = {Role.ROLE_STUDENT})
     public ResponseEntity<BaseResponse<StudentResponseDto>> getStudent(@SessionUser User user) {
         return ResponseFactory.ok(studentGetByTokenService.cu71GetStudentByToken(user),SuccessfulMessages.okSuccessfully());
     }
