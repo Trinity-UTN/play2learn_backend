312ec69d94f22cec494434ff122b86d0d8a75d8f
Juan Pablo Dealbera <juanpidealbera@gmail.com>
2025-10-16 16:25:50 -0300

    feat List paginated aspect

--- src/main/java/trinity/play2learn/backend/profile/avatar/repositories/IAspectRepository.java
+++ src/main/java/trinity/play2learn/backend/profile/avatar/repositories/IAspectRepository.java
@@ -2,11 +2,12 @@ package trinity.play2learn.backend.profile.avatar.repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 import trinity.play2learn.backend.profile.avatar.models.Aspect;
 
-public interface IAspectRepository extends CrudRepository<Aspect, Long> {
+public interface IAspectRepository extends CrudRepository<Aspect, Long>, JpaSpecificationExecutor<Aspect> {
 
     boolean existsByName(String name);
     
