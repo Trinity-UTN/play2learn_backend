a8b1145293e5576a359533ef0b3ba66f11124abd
felipedelzoppo <felipedelzoppo@gmail.com>
2025-10-24 17:29:43 -0300

    feat: Endpoint para eliminar un beneficio. #257

---
+++ src/main/java/trinity/play2learn/backend/benefits/services/interfaces/IBenefitDeleteService.java
@@ -0,0 +1,8 @@
+package trinity.play2learn.backend.benefits.services.interfaces;
+
+import trinity.play2learn.backend.user.models.User;
+
+public interface IBenefitDeleteService {
+    
+    void cu94DeleteBenefit(User user, Long id);
+}
