d1996419d9e74e61791eed674f3205344f678e47
felipedelzoppo <felipedelzoppo@gmail.com>
2025-10-27 12:35:53 -0300

    fix: Notacion sessionRequired en listar usos de beneficio de estudiante

--- src/main/java/trinity/play2learn/backend/benefits/controllers/BenefitListUsedByStudentController.java
+++ src/main/java/trinity/play2learn/backend/benefits/controllers/BenefitListUsedByStudentController.java
@@ -10,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.AllArgsConstructor;
 import trinity.play2learn.backend.benefits.dtos.benefitPurchase.BenefitPurchasedUsedResponseDto;
 import trinity.play2learn.backend.benefits.services.interfaces.IBenefitListUsedByStudentService;
+import trinity.play2learn.backend.configs.annotations.SessionRequired;
 import trinity.play2learn.backend.configs.annotations.SessionUser;
 import trinity.play2learn.backend.configs.messages.SuccessfulMessages;
 import trinity.play2learn.backend.configs.response.BaseResponse;
 import trinity.play2learn.backend.configs.response.ResponseFactory;
+import trinity.play2learn.backend.user.models.Role;
 import trinity.play2learn.backend.user.models.User;
 
 @RestController
@@ -24,6 +26,7 @@ public class BenefitListUsedByStudentController {
     private final IBenefitListUsedByStudentService benefitListUsedByStudentService;
 
     @GetMapping("/used")
+    @SessionRequired(roles = {Role.ROLE_STUDENT})
     public ResponseEntity<BaseResponse<List<BenefitPurchasedUsedResponseDto>>> list(@SessionUser User user) {
         return ResponseFactory.ok(benefitListUsedByStudentService.cu93ListUsedByStudent(user), SuccessfulMessages.okSuccessfully());
     }
