ded1f05215a5a3f3ba6801752247a53ef4b46ab4
felipedelzoppo <felipedelzoppo@gmail.com>
2025-11-05 17:32:03 -0300

    feat: Agregue filtrado por expirados en listar beneficios de docente paginado

--- src/main/java/trinity/play2learn/backend/benefits/specs/BenefitSpecs.java
+++ src/main/java/trinity/play2learn/backend/benefits/specs/BenefitSpecs.java
@@ -1,5 +1,7 @@
 package trinity.play2learn.backend.benefits.specs;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.domain.Specification;
 
 import trinity.play2learn.backend.benefits.models.Benefit;
@@ -19,12 +21,16 @@ public class BenefitSpecs {
     // Filtro dinámico: cualquier campo = valor exacto
     public static Specification<Benefit> genericFilter(String field, String value) {
 
-        if (field.equals("subjectId")) {
-            try {
-                return hasSubjectId(Long.valueOf(value));
-            } catch (Exception e) { // Evita un 500 si el valor no es un numero
-                return (root, query, cb) -> cb.conjunction();
-            }
+        switch (field) {
+            case "subjectId":
+                try {
+                    return hasSubjectId(Long.valueOf(value));
+                } catch (Exception e) { // Evita un 500 si el valor no es un numero
+                    return (root, query, cb) -> cb.conjunction();
+                }
+
+            case "state":
+                return hasState(value);
         }
 
         return (root, query, cb) -> {
@@ -43,6 +49,23 @@ public class BenefitSpecs {
                 return cb.conjunction();
             }
             return cb.equal(root.get("subject").get("id"), subjectId);
+        };
+    }
+
+    public static Specification<Benefit> hasState(String state) {
+        return (root, query, cb) -> {
+            LocalDateTime now = LocalDateTime.now();
+
+            switch (state) {
+                case "PUBLISHED":
+                    return cb.greaterThanOrEqualTo(root.get("endAt"), now);
+
+                case "EXPIRED":
+                    return cb.lessThan(root.get("endAt"), now);
+
+                default:
+                    return cb.conjunction(); // si no matchea, no aplica filtro
+            }
         };
     }
 
