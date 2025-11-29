6093d4eedd4c72de3d99acd217b5e740d07c7317
felipedelzoppo <felipedelzoppo@gmail.com>
2025-11-29 16:38:28 -0300

    feat: Servicio automatico para generar notificaciones de actividad. #322

---
+++ src/main/java/trinity/play2learn/backend/notification/services/commons/NotificationCreateByUsersService.java
@@ -0,0 +1,27 @@
+package trinity.play2learn.backend.notification.services.commons;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import trinity.play2learn.backend.notification.mappers.NotificationMapper;
+import trinity.play2learn.backend.notification.models.NotificationType;
+import trinity.play2learn.backend.notification.repositories.INotificationRepository;
+import trinity.play2learn.backend.notification.services.interfaces.INotificationCreateByUsersService;
+import trinity.play2learn.backend.user.models.User;
+
+@Service
+@AllArgsConstructor
+public class NotificationCreateByUsersService implements INotificationCreateByUsersService {
+
+    private final INotificationRepository notificationRepository;
+
+    @SuppressWarnings("null")
+    @Override
+    public void createUsersNotifications(List<User> users, NotificationType notificationType) {
+        users.forEach(user -> {
+            notificationRepository.save(NotificationMapper.toModel(notificationType, user));
+        });
+    }
+}
---
+++ src/main/java/trinity/play2learn/backend/notification/services/interfaces/INotificationCheckByActivitiesService.java
@@ -0,0 +1,6 @@
+package trinity.play2learn.backend.notification.services.interfaces;
+
+public interface INotificationCheckByActivitiesService {
+
+    void cu116CheckActivityNotifications();
+}
