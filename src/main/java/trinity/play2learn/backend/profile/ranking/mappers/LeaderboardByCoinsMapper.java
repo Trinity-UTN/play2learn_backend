72f4ac69d578545a96d5a577992dc3df640facdb
Juan Pablo Dealbera <80698781+juanpid2112@users.noreply.github.com>
2026-02-05 22:58:35 -0300

    feat Avatar en los rankings

--- src/main/java/trinity/play2learn/backend/profile/ranking/mappers/LeaderboardByCoinsMapper.java
+++ src/main/java/trinity/play2learn/backend/profile/ranking/mappers/LeaderboardByCoinsMapper.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import trinity.play2learn.backend.economy.wallet.models.Wallet;
+import trinity.play2learn.backend.profile.avatar.mappers.AspectMapper;
 import trinity.play2learn.backend.profile.ranking.dtos.response.LeaderboardParticipantResponseDto;
 import trinity.play2learn.backend.profile.ranking.dtos.response.LeaderboardResponseDto;
 
@@ -14,6 +15,9 @@ public class LeaderboardByCoinsMapper {
             .position(position)
             .name(wallet.getStudent().getLastname()+ " " + wallet.getStudent().getName())
             .quantity(Math.round((wallet.getBalance()+wallet.getInvertedBalance()) * 100.0) / 100.0)
+            .selectedBody(wallet.getStudent().getProfile().getSelectedBody() != null ? AspectMapper.toSimpleDto(wallet.getStudent().getProfile().getSelectedBody()) : null)
+            .selectedShirt(wallet.getStudent().getProfile().getSelectedShirt() != null ? AspectMapper.toSimpleDto(wallet.getStudent().getProfile().getSelectedShirt()) : null)
+            .selectedHat(wallet.getStudent().getProfile().getSelectedHat() != null ? AspectMapper.toSimpleDto(wallet.getStudent().getProfile().getSelectedHat()) : null)
             .build();
     }
 
